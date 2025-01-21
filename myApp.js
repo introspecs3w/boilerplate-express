2c101925c0cf5bafaeb70d48f51319c7344d41a7
Faizan Ahmed <faizan.w3@gmail.com>
2025-01-21 14:17:09 +0500

    challenge 1

--- myApp.js
+++ myApp.js
@@ -2,13 +2,16 @@ let express = require('express');
 let app = express();
 
 
+function functionGet(req, res)
+{
+    res.send("Hello Express");
+}
 
 
+console.log("Hello World");
 
 
-
-
-
+app.get('/', functionGet)
 
 
 
